2061010b674d5eebc7ed290991efecd231f0620c
Pablo Vinicius <vinicius.pablo.18@gmail.com>
2018-09-19 20:35:21 -0300

    fixed back button on politician page

--- pages/search.js
+++ pages/search.js
@@ -11,6 +11,7 @@ import {
     EvidenceText
 } from '../styles/SearchPageStyles';
 import API from '../general/Api';
+import { format } from 'url';
 
 
 export default class Search extends Component {
@@ -26,6 +27,7 @@ export default class Search extends Component {
     }
 
     static async getInitialProps(context) {
+        console.log('Search')
         const { text, state, terms } = context.query;
         let query, data = {}, method='GET';
         if(terms === undefined) {
@@ -34,12 +36,15 @@ export default class Search extends Component {
             query = 'politicos?';
             data = {
                 terms: terms.split(','),
-                page: 1
+                page: 0
             };
             method='POST'
         }
 
         try {
+            console.log('my method', method)
+            console.log('URL initial', `${query}page=0`)
+            console.log('Body initial', data)
             const response = await API({ method, url: `${query}page=0`, data });
             return { reserachText: text, ...response.data, status: response.status, state, method, query, data };
         } catch (error) {
@@ -49,6 +54,14 @@ export default class Search extends Component {
     }
 
     requestNewPage = async page => {
+        let formData = new FormData();
+        formData.append('terms', this.state.data.terms);
+        formData.append('page', this.state.data.page);
+
+        console.log('my form data', formData)
+
+        console.log('Url', `${this.state.query}page=${page.selected}`)
+        console.log('Body', this.state.data)
         API({ method: this.state.method, url: `${this.state.query}page=${page.selected}`,  ...this.state.data })
         .then((response) => {
             this.setState({ ...response.data, currentPage: page.selected })
