46a8ba8a7a0885d3e24259b4eb7963d9684c3b43
Pablo Vinicius <vinicius.pablo.18@gmail.com>
2018-09-24 23:39:09 -0300

    resolving null frequency error

--- components/ChairChart.js
+++ components/ChairChart.js
@@ -25,87 +25,94 @@ export default class ChairChart extends PureComponent {
 
     constructor(props) {
         super(props);
-        this.presence = Math.round((this.props.presence/this.props.total)*20);
-        this.justifiedAbsence = Math.round((this.props.justifiedAbsence/this.props.total)*20);
-        this.unjustifiedAbsence = Math.round((this.props.unjustifiedAbsence/this.props.total)*20);
+        if(this.props.frequency) {
+            this.presence = Math.round((this.props.frequency.presence/this.props.frequency.total)*20);
+            this.justifiedAbsence = Math.round((this.props.frequency.justifiedAbsence/this.props.frequency.total)*20);
+            this.unjustifiedAbsence = Math.round((this.props.frequency.unjustifiedAbsence/this.props.frequency.total)*20);
 
-        this.legend = [
-            {
-                color: '#97C7F6',
-                name: 'Total de sessões',
-                value: this.props.total
-            },
-            {
-                color: '#00B894',
-                name: 'Presenças confirmadas',
-                value: this.props.presence
-            },
-            {
-                color: '#A07A00',
-                name: 'Ausências justificadas',
-                value: this.props.justifiedAbsence
-            },
-            {
-                color: '#E17055',
-                name: 'Ausências não justificadas',
-                value: this.props.unjustifiedAbsence
-            },
-        ];
+            this.legend = [
+                {
+                    color: '#97C7F6',
+                    name: 'Total de sessões',
+                    value: this.props.frequency.total
+                },
+                {
+                    color: '#00B894',
+                    name: 'Presenças confirmadas',
+                    value: this.props.frequency.presence
+                },
+                {
+                    color: '#A07A00',
+                    name: 'Ausências justificadas',
+                    value: this.props.frequency.justifiedAbsence
+                },
+                {
+                    color: '#E17055',
+                    name: 'Ausências não justificadas',
+                    value: this.props.frequency.unjustifiedAbsence
+                },
+            ];
 
-        this.chairLegend = [
-            {
-                chair: greenChair,
-                title: 'Presença confirmada'
-            },
-            {
-                chair: yellowChair,
-                title: 'Ausência justificada'
-            },
-            {
-                chair: redChair,
-                title: 'Ausência não justificada'
-            }
-        ];
+            this.chairLegend = [
+                {
+                    chair: greenChair,
+                    title: 'Presença confirmada'
+                },
+                {
+                    chair: yellowChair,
+                    title: 'Ausência justificada'
+                },
+                {
+                    chair: redChair,
+                    title: 'Ausência não justificada'
+                }
+            ];
+        }
     }
 
 
     render() {
         return(
+            this.props.frequency ?
+                <ChairChartContainer>
+                    <ChartTitle>{this.props.title}</ChartTitle>
+                    <ChairWrapper>
+                        {Array(this.presence).fill(0).map((element, index) => (
+                            <ChairIcon key={index} src={greenChair} />
+                        ))}
+                        {Array(this.justifiedAbsence).fill(0).map((element, index) => (
+                            <ChairIcon key={index} src={yellowChair} />
+                        ))}
+                        {Array(this.unjustifiedAbsence).fill(0).map((element, index) => (
+                            <ChairIcon key={index} src={redChair} />
+                        ))}
+                    </ChairWrapper>
+                    <LegendContainer>
+                        {
+                            this.legend.map((element, index) => (
+                                <LegendLine key={index}>
+                                    <Dot backgroundColor={element.color} />
+                                    <LegendName>
+                                        {element.name} <LegendValue>{element.value} sessões</LegendValue>
+                                    </LegendName>
+                                </LegendLine>
+                            ))
+                        }
+                    </LegendContainer>
+                    <ChairLegendWrapper>
+                        {
+                            this.chairLegend.map((element, index) => (
+                                <ChairLegendLine key={index}>
+                                    <ChairIco src={element.chair} />
+                                    <ChairLegendText>{element.title}</ChairLegendText>
+                                </ChairLegendLine>
+                            ))
+                        }
+                    </ChairLegendWrapper>
+                </ChairChartContainer>
+            :
             <ChairChartContainer>
-                <ChartTitle>{this.props.title}</ChartTitle>
-                <ChairWrapper>
-                    {Array(this.presence).fill(0).map((element, index) => (
-                        <ChairIcon key={index} src={greenChair} />
-                    ))}
-                    {Array(this.justifiedAbsence).fill(0).map((element, index) => (
-                        <ChairIcon key={index} src={yellowChair} />
-                    ))}
-                    {Array(this.unjustifiedAbsence).fill(0).map((element, index) => (
-                        <ChairIcon key={index} src={redChair} />
-                    ))}
-                </ChairWrapper>
-                <LegendContainer>
-                    {
-                        this.legend.map((element, index) => (
-                            <LegendLine key={index}>
-                                <Dot backgroundColor={element.color} />
-                                <LegendName>
-                                    {element.name} <LegendValue>{element.value} sessões</LegendValue>
-                                </LegendName>
-                            </LegendLine>
-                        ))
-                    }
-                </LegendContainer>
-                <ChairLegendWrapper>
-                    {
-                        this.chairLegend.map((element, index) => (
-                            <ChairLegendLine key={index}>
-                                <ChairIco src={element.chair} />
-                                <ChairLegendText>{element.title}</ChairLegendText>
-                            </ChairLegendLine>
-                        ))
-                    }
-                </ChairLegendWrapper>
+                <ChartTitle>Frequência não informada</ChartTitle>
             </ChairChartContainer>
         );
     }
